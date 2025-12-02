e450650b6e4237286ee55e375a2aacbc22ad214d
Mahendra prasath.s <mahendraprasath.553@gmail.com>
2025-12-02 10:29:32 +0530

    mongodb database connected with backend folder structure

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,17 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
-// const studentRoutes = require("./routes/studentRoutes");
-// const dashboardRoutes = require("./routes/dashboardRoutes");
+const connectDB = require("./config/db");
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// MongoDB connection
-// mongoose
-//   .connect(process.env.MONGO_URI)
-//   .then(() => console.log("MongoDB Connected"))
-//   .catch((err) => console.log("MongoDB Error:", err));
+connectDB();
 
-app.get("/", (req, res) => {
-  res.send("Backend running...");
-});
-
-// // Routes
-// app.use("/api/students", studentRoutes);
-// app.use("/api/dashboard", dashboardRoutes);
 
 app.listen(5000, () => {
   console.log("Server running on port 5000");
